9d4ad87545e051bc273795ef9c442909dda3baac
ali.abukahil <ali.abukahil@gmail.com>
2021-12-14 08:05:38 +0100

    Styles, Attributes, and Classes

--- script.js
+++ script.js
@@ -70,3 +70,55 @@ document
     // old way of removing element from DOM before .remove() method
     // message.parentElement.removeChild(message);
   });
+
+// Styles, Attributes and Classes
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+// resetting the attributes
+logo.alt = "Beautiful minimalist logo";
+console.log(logo.alt);
+
+// NON-standerd Attributes that's why it is not workin' & showing undefined result in Console
+console.log(logo.designer);
+// another way of reading this value from the DOM
+console.log(logo.getAttribute("designer"));
+
+logo.setAttribute("company", "Bankist");
+
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add("c", "j");
+logo.classList.remove("c", "j");
+logo.classList.toggle("c");
+logo.classList.contains("c"); // not includes
+
+// Don't use, cuz this will override all the exiting classes &&
+// also it allows us to only put one class
+logo.className = "jonas";
