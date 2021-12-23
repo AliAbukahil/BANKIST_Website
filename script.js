03e2a63355c7f7d20c0d136ed61b5b563c2371b7
ali.abukahil <ali.abukahil@gmail.com>
2021-12-23 23:21:20 +0100

    Efficient Script Loading defer and async

--- script.js
+++ script.js
@@ -539,3 +539,17 @@ document.querySelector(".nav").addEventListener("click", function (e) {
 // });
 
 // Building a tabbed Components
+
+// document.addEventListener("DOMContentLoaded", function (e) {
+//   console.log("HTML parsed and DOM tree built", e);
+// });
+
+// window.addEventListener("load", function (e) {
+//   console.log("Page fully loaded", e);
+// });
+
+// window.addEventListener("beforeunload", function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = "";
+// });
